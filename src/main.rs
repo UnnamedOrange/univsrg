3725ee3330efe4f0ad9e248d7710fe6df3385515
UnnamedOrange <lycheng1215@sina.com>
2023-08-29 22:07:00 +0800

    Parse command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 mod univsrg;
 
-fn main() {}
+use clap::{ArgAction, Parser};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Input files.
+    /// Support extensions include `.osz`.
+    #[arg(action = ArgAction::Append)]
+    inputs: Vec<String>,
+
+    /// Output file.
+    /// Supported extensions include `.osz`.
+    #[arg(short)]
+    output: String,
+}
+
+fn main() {
+    let args = Args::parse();
+}
