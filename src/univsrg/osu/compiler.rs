844caa3a5037ca65c7665005e5426b03a4c3c8bd
UnnamedOrange <lycheng1215@sina.com>
2023-09-06 14:01:24 +0800

    Use ResourceOut

--- src/univsrg/osu/compiler.rs
+++ src/univsrg/osu/compiler.rs
@@ -2,14 +2,20 @@ use std::{io, path::Path};
 
 use tempfile::{tempdir, TempDir};
 
-use super::super::{traits::ToOsu, types::Package};
+use super::super::{resource::ResourceOut, traits::ToOsu, types::Package};
 
 impl ToOsu for Package {
     fn to_osu(&self, path: &Path) -> io::Result<()> {
         let temp_dir: TempDir = tempdir()?;
-        // TODO: Remap and settle resources.
+
+        // Remap and settle resources.
+        let mut resource_out = ResourceOut::new();
+        resource_out.inflate(temp_dir.path().to_owned(), &self.resource_pool)?;
+
         // TODO: Compile beatmaps.
+
         // TODO: Package all files to a bundle.
+
         Ok(())
     }
 }
