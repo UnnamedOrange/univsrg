c3caffdab29f7b6b5158d39187226ca7a355457b
UnnamedOrange <lycheng1215@sina.com>
2023-09-10 14:08:08 +0800

    Update compiler.rs

--- src/univsrg/osu/compiler.rs
+++ src/univsrg/osu/compiler.rs
@@ -1,8 +1,22 @@
-use std::{io, path::Path};
+use std::{
+    fs::File,
+    io,
+    path::{Path, PathBuf},
+};
 
 use tempfile::{tempdir, TempDir};
 
-use super::super::{resource::ResourceOut, traits::ToOsu, types::Package};
+use super::super::{resource::ResourceOut, traits::ToOsu, types::Beatmap, types::Package};
+
+fn compile_beatmap(beatmap: &Beatmap, root: &Path, resource: &ResourceOut) -> io::Result<()> {
+    let basename = PathBuf::from(beatmap.make_basename());
+    let filename: PathBuf = [root, &basename].iter().collect();
+
+    let mut output = File::create(filename)?;
+    // TODO: Generate the file.
+
+    Ok(())
+}
 
 impl ToOsu for Package {
     fn to_osu(&self, path: &Path) -> io::Result<()> {
@@ -12,7 +26,10 @@ impl ToOsu for Package {
         let mut resource_out = ResourceOut::new();
         resource_out.inflate(temp_dir.path().to_owned(), &self.resource_pool)?;
 
-        // TODO: Compile beatmaps.
+        for beatmap in &self.beatmaps {
+            // TODO: Handle error.
+            compile_beatmap(beatmap, temp_dir.path(), &resource_out)?;
+        }
 
         // TODO: Package all files to a bundle.
 
