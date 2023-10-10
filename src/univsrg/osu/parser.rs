88b7e2554942608a8d3a71795df14af39ac7fd5b
UnnamedOrange <lycheng1215@sina.com>
2023-10-10 09:46:36 +0800

    Write outline

--- src/univsrg/osu/parser.rs
+++ src/univsrg/osu/parser.rs
@@ -7,6 +7,10 @@ use super::{
 
 impl AppendToUnivsrg for OszPath {
     fn append_to_univsrg(&self, package: &mut Package) -> io::Result<()> {
+        // TODO: Unzip osz file.
+
+        // TODO: Enumerate osu files and parse.
+
         Ok(())
     }
 }
