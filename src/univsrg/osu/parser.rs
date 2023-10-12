b255f2a3e5f72bce59abb5408c62644c9f9e13ad
UnnamedOrange <lycheng1215@sina.com>
2023-10-12 08:53:28 +0800

    Unzip osz file

--- src/univsrg/osu/parser.rs
+++ src/univsrg/osu/parser.rs
@@ -1,5 +1,8 @@
-use std::fs::File;
-use std::io;
+use std::{
+    fs::{create_dir_all, File},
+    io,
+    path::Path,
+};
 
 use tempfile::{tempdir, TempDir};
 use zip::ZipArchive;
@@ -11,10 +14,30 @@ use super::{
 
 impl AppendToUnivsrg for OszPath {
     fn append_to_univsrg(&self, package: &mut Package) -> io::Result<()> {
-        // TODO: Unzip osz file.
+        // Unzip osz file.
         let source_dir: TempDir = tempdir()?;
         let zip_file = File::open(&self.osz_path)?;
-        let zip = ZipArchive::new(zip_file)?;
+        let mut zip = ZipArchive::new(zip_file)?;
+
+        // https://blog.csdn.net/m0_47202518/article/details/120421870
+        for idx in 0..zip.len() {
+            let mut file = zip.by_index(idx)?;
+            let out_path = if let Some(inner_path) = file.enclosed_name() {
+                Path::join(source_dir.path(), inner_path)
+            } else {
+                continue;
+            };
+
+            if !file.name().ends_with('/') {
+                if let Some(parent) = out_path.parent() {
+                    if !parent.exists() {
+                        create_dir_all(parent)?;
+                    }
+                    let mut out_file = File::create(&out_path)?;
+                    io::copy(&mut file, &mut out_file)?;
+                }
+            }
+        }
 
         // TODO: Enumerate osu files and parse.
 
